229631badb5fa214e5677e8d261b784b8a37200b
btran54 <btran54@ucsc.edu>
2024-07-05 21:30:44 -0700

    Added a Dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Register from './Pages/Register'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./Pages/Register";
 import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/register' element={<Signup />}></Route>
         <Route path='/' element={<Login />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
       </Routes>
     </BrowserRouter>
   );
